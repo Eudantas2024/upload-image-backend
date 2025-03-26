7feb2acc8b789f06a588a92980c145fc6177f071
EUDANTAS RODRIGUES ALVES <eudantas.ralves@senacsp.edu.br>
2025-03-25 21:50:39 -0300

    upload image 25032025

---
+++ controllers/PictureControllers.js
@@ -0,0 +1,26 @@
+const Picture = require("../models/Picture")
+
+const fs = require("fs")
+
+exports.create = async (req, res) => {
+    res.json("OK!");
+
+    try{
+        const { name } = req.body;
+
+        const file = req.file;
+
+        const picture = new Picture({
+            name,
+            scr: file.path,
+        });
+    
+
+    await picture.save();
+
+    res.json({ picture, msg: "Imagem salva com sucesso!" });
+} catch (error) {
+    res.status(500).json({ message: "Erro ao salvar!" });
+}
+};
+
